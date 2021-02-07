f162d99ee867a7a44cc3f5f8ddc05d7ceb3f2557
Sneha Sridharan <66522297+Sneha-Sridharan@users.noreply.github.com>
2021-02-07 12:44:17 +0530

    Delete 041) First Missing Positive.py

--- 041) First Missing Positive.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def firstMissingPositive(self, nums):
-        flag=0
-        n=1
-        while flag==0:
-            if n not in nums:
-                flag=n
-            n=n+1
-        return flag
